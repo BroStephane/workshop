5cb4ed07c1c7b4e1ced81ff73827a874b9bd8780
Méline <gmeline@MacBook-Pro-VR.local>
2024-10-10 11:18:21 +0200

    mg-add accueil Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Connexion from './components/connexion';
+import Accueil from './components/accueil';
+
 
 function App() {
   return (
-    <div className="App">
-      <Connexion />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/connexion" element={<Connexion />} /> {/* Page de connexion */}
+          <Route path="/accueil" element={<Accueil/>}/>{/* Page d'accueil */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ frontend/src/components/accueil.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './accueil.css';
+import logo from '../assets/images/MyHealthSync.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faSquarePlus, faPaperPlane, faUser } from '@fortawesome/free-solid-svg-icons'; // Si toutes sont solid
+
+
+const Accueil = () => {
+    return (
+        <div className="accueil-container">
+            <header className="logo-container">
+                <a href="/accueil"><img src={logo} alt="MyHealthSync Logo" className="logo" /></a>
+            </header>
+
+            <body>
+                <div className="accueil-header">
+                    <h1>Bonjour Samuel !</h1>
+                </div>
+                <div className="graphique-container">
+                    <div className='rectangleGraphique'>
+                        <h1 className='titreGraphique'>Le graphique arrive...</h1>
+                    </div>
+                </div>
+                <div className="medications-container">
+                    <h2 className="titleCheckList">A prendre :</h2>
+                    <div className="rectangle1">
+                        <input type="checkbox" className="checkBox1" id="checkBox1" />
+                        <label htmlFor="checkBox1" className="checkBoxLabel">
+                            Ibuprofène 
+                            <span className="checkmarkIcon"><i class="fa-regular fa-xmark"></i></span>
+                        </label>
+                        <p className="time1">10:00</p>
+                    </div>
+                    <div className="rectangle2">
+                        <input type="checkbox" className="checkBox2" id="checkBox2" />
+                        <label htmlFor="checkBox1" className="checkBoxLabel">
+                            Doliprane
+                            <span className="checkmarkIcon"><i class="fa-regular fa-xmark"></i></span>
+                        </label>
+                        <p className="time2">12:00</p>
+                    </div>
+                    <div className="rectangle3">
+                        <input type="checkbox" className="checkBox3" id="checkBox3" />
+                        <label htmlFor="checkBox1" className="checkBoxLabel">
+                        Spasfon
+                            <span className="checkmarkIcon"><i class="fa-regular fa-xmark"></i></span>
+                        </label>
+                        <p className="time3">14:00</p>
+                    </div>
+                </div>
+            </body>
+            
+            <footer className="accueil-footer">
+                <div className="rectangleBasDePage">
+                <nav>
+                    <a href="/accueil"><FontAwesomeIcon icon={faHouse} className='footer-icon'/></a>
+                    <a href="#"><FontAwesomeIcon icon={faSquarePlus} className='footer-icon'/></a>
+                    <a href="#"><FontAwesomeIcon icon={faPaperPlane} className='footer-icon'/></a>
+                    <a href="#"><FontAwesomeIcon icon={faUser} className='footer-icon'/></a>
+                </nav>
+
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Accueil;
